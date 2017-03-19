bf87db6c10814e6ff253a36a74039ff086d92fb3
immanoj16 <mmaharana14@gmail.com>
2017-03-19 15:36:06 -0700

    Add: Band model

--- band/models.py
+++ band/models.py
@@ -1,5 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class Band(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    image = models.CharField(max_length=250, null=True, blank=True)
+    date_added = models.DateTimeField(default=datetime.datetime.now())
+    date_formed = models.DateField(null=True, blank=True)
+    date_disbanded = models.DateField(null=True, blank=True)
+    origin = models.CharField(max_length=250, null=True, blank=True)
+    bio = models.TextField(max_length=5000, null=True, blank=True)
